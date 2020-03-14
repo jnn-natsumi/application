cb20ef0fbb7f369a28090b8fc1d750818a15487c
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-03-14 22:27:17 +0900

    [finish]4-4,5,6,7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,18 +1,37 @@
 class BlogsController < ApplicationController
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
+    # 記事を全件取得
+    @blogs = Blog.all
   end
 
   def new
   	@blog = Blog.new
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blog_path(blog.id)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
